130f1f2f325598afabcbeb672034e619e6178093
rose pose <rosepose@gmail.com>
2025-07-30 20:53:21 +0330

    fix: a tiny refactor applied in replacement module

--- source/pyggi/mutation/replacement.py
+++ source/pyggi/mutation/replacement.py
@@ -16,16 +16,16 @@ class ReplacementOperator:
     def __init__(self, ast, node):
         self.utils = MutationUtils()
         self.operators = [
-            self.safe_wrap_raw_pointers,
-            self.safe_wrap_raw_pointer_argumetns,
-            self.make_global_static_pointers_unmutable,
-            self.move_ast_node,
-            self.shrink_unsafe_block_stmts,
+            # self.safe_wrap_raw_pointers,
+            # self.safe_wrap_raw_pointer_argumetns,
+            # self.make_global_static_pointers_unmutable,
+            # self.move_ast_node,
+            # self.shrink_unsafe_block_stmts,
             self.flip_mutabilities,
-            self.safe_wrap_struct_field,
-            self.replace_raw_dereferences_in_unsafe_block,
+            # self.safe_wrap_struct_field,
+            # self.replace_raw_dereferences_in_unsafe_wrapper,
         ]
-        self.new_ast = self.apply_random_mutations(ast, node, 3)
+        self.new_ast = self.apply_random_mutations(ast, node, 1)
 
     def apply_random_mutations(self, ast, node, num_ops):
         selected_ops = random.sample(self.operators, k=num_ops)
@@ -36,12 +36,12 @@ class ReplacementOperator:
     def get_new_ast(self):
         return self.new_ast
 
-    def replace_raw_dereferences_in_unsafe_block(self, ast_root, target_node):
+    def transform_let_stmt(self, ast_root, target_node, transform_fn, label="transform"):
         parents = self.utils.get_all_parents(ast_root, target_node)
         if not isinstance(ast_root, Program):
             return None
 
-        print("replace_raw_dereferences_in_unsafe_block!")
+        print(f"{label}!")
         remaining_tops = []
         parent_len = len(parents)
 
@@ -52,10 +52,7 @@ class ReplacementOperator:
 
             parent_1, parent_2 = parents[-2], parents[-3]
             top_type_matches = isinstance(parent_1, type(top))
-            if isinstance(top, list):
-                top_children = top
-            else:
-                top_children = top.getChildren()
+            top_children = top if isinstance(top, list) else top.getChildren()
 
             if not top_type_matches:
                 remaining_tops.append(top)
@@ -63,97 +60,95 @@ class ReplacementOperator:
 
             if isinstance(parent_2, type(top_children)):
                 new_stmts = []
-                if isinstance(top_children, Block):
-                    for stmt in top_children.getChildren():
-                        if self.utils.statements_eq(stmt, target_node) and isinstance(stmt, LetStmt):
-                            if len(stmt.var_defs) == 1:
-                                if isinstance(stmt.values[0], DereferenceExpr):
-                                    new_let_stmt = LetStmt(var_defs=VarDef(name=stmt.var_defs[0].name,
-                                        mutable= stmt.var_defs[0].mutable,
-                                        by_ref=stmt.var_defs[0].by_ref,
-                                        var_type=RefType("T")), values=UnsafeExpression(expr=ReferenceExpr(expr=stmt.values[0])))
-                                    new_stmts.append(new_let_stmt)
-                        else:
-                            new_stmts.append(stmt)
-                    top_children.setBody(new_stmts)
-                    top.setBody(top_children)
 
-                elif isinstance(top_children, FunctionDef) and isinstance(top_children.body, Block):
-                    for stmt in top_children.body.getChildren():
-                        if self.utils.statements_eq(stmt, target_node) and isinstance(stmt, LetStmt):
-                            if len(stmt.var_defs) == 1:
-                                if isinstance(stmt.values[0], DereferenceExpr):
-                                    new_let_stmt = LetStmt(var_defs=VarDef(name=stmt.var_defs[0].name,
-                                        mutable= stmt.var_defs[0].mutable,
-                                        by_ref=stmt.var_defs[0].by_ref,
-                                        var_type=RefType("T")), values=UnsafeExpression(expr=ReferenceExpr(expr=stmt.values[0])))
-                                    new_stmts.append(new_let_stmt)
+                block = top_children.body if isinstance(top_children, FunctionDef) else top_children
+                if isinstance(block, Block):
+                    for stmt in block.getChildren():
+                        if self.utils.statements_eq(stmt, target_node):
+                            new_stmt = transform_fn(stmt)
+                            new_stmts.append(new_stmt if new_stmt else stmt)
                         else:
                             new_stmts.append(stmt)
-                    top_children.body.setBody(new_stmts)
-                    top.setBody(top_children)
-                remaining_tops.append(top)
-        
-        print("unsafe dereference: ", pretty_print_ast(Program(items=remaining_tops)))
+
+                    block.setBody(new_stmts)
+                    if isinstance(top_children, FunctionDef):
+                        top_children.body = block
+                    else:
+                        top.setBody(block)
+
+                top.setBody(top_children)
+            remaining_tops.append(top)
+
+        print(f"{label} result:", pretty_print_ast(Program(items=remaining_tops)))
         return Program(items=remaining_tops)
+    
+    def replace_raw_dereferences_in_unsafe_wrapper(self, ast_root, target_node):
+        def transform(stmt):
+            if isinstance(stmt, LetStmt) and len(stmt.var_defs) == 1:
+                val = stmt.values[0]
+                if isinstance(val, DereferenceExpr):
+                    return LetStmt(
+                        var_defs=VarDef(
+                            name=stmt.var_defs[0].name,
+                            mutable=stmt.var_defs[0].mutable,
+                            by_ref=stmt.var_defs[0].by_ref,
+                            var_type=RefType("T")
+                        ),
+                        values=UnsafeExpression(expr=ReferenceExpr(expr=val))
+                    )
+            return None  # No change
+        return self.transform_let_stmt(ast_root, target_node, transform, label="replace_raw_dereferences_in_unsafe_wrapper")
 
     def flip_mutabilities(self, ast_root, target_node):
-        parents = self.utils.get_all_parents(ast_root, target_node)
+        def transform(stmt):
+            if isinstance(stmt, LetStmt) and len(stmt.var_defs) == 1:
+                return LetStmt(
+                    var_defs=VarDef(
+                        name=stmt.var_defs[0].name,
+                        mutable=not stmt.var_defs[0].mutable,
+                        by_ref=stmt.var_defs[0].by_ref,
+                        var_type=stmt.var_defs[0].type
+                    ),
+                    values=stmt.values[0]
+                )
+            return None
+        return self.transform_let_stmt(ast_root, target_node, transform, label="flip_mutabilities")
+
+    def safe_wrap_struct_field(self, ast_root, target_node):
         if not isinstance(ast_root, Program):
             return None
-
-        print("flipping!")
         remaining_tops = []
-        parent_len = len(parents)
-
         for top in ast_root.getChildren():
-            if parent_len < 3:
-                remaining_tops.append(top)
-                continue
+            new_fields = []
+            if isinstance(top, StructDef):
+                for field in top.getChildren():
+                    if isinstance(field.type, PointerType) and field.type.mutability:
+                        new_field = StructField(name=field.name, typeExpr=SafeNonNullWrapper(
+                            typeExpr=field.type), visibility=field.visibility)
+                        new_fields.append(new_field)
+                    else:
+                        new_fields.append(field)
+                top.setChildren(new_fields)
 
-            parent_1, parent_2 = parents[-2], parents[-3]
-            top_type_matches = isinstance(parent_1, type(top))
-            if isinstance(top, list):
-                top_children = top
-            else:
-                top_children = top.getChildren()
+            elif isinstance(top, FunctionDef) and isinstance(top.body, Block):
+                new_Stmts = []
+                for stmt in top.body.getChildren():
+                    if isinstance(stmt, StructDef):
+                        for field in stmt.getChildren():
+                            if isinstance(field.type, PointerType) and field.type.mutability:
+                                new_field = StructField(name=field.name, typeExpr=SafeNonNullWrapper(
+                                    typeExpr=field.type), visibility=field.visibility)
+                                new_fields.append(new_field)
+                            else:
+                                new_fields.append(field)
+                        new_Stmts.append(new_field)
+                    else:
+                        new_Stmts.append(stmt)
+                new_block = Block(stmts=new_Stmts, isUnsafe=top.body.isUnsafe)
+                top.setBody(new_block)
+            remaining_tops.append(top)
 
-            if not top_type_matches:
-                remaining_tops.append(top)
-                continue
-
-            if isinstance(parent_2, type(top_children)):
-                new_stmts = []
-                if isinstance(top_children, Block):
-                    for stmt in top_children.getChildren():
-                        if self.utils.statements_eq(stmt, target_node):
-                            if len(stmt.var_defs) == 1:
-                                new_let_stmt = LetStmt(var_defs=VarDef(name=stmt.var_defs[0].name,
-                                    mutable= not stmt.var_defs[0].mutable,
-                                    by_ref=stmt.var_defs[0].by_ref,
-                                    var_type=stmt.var_defs[0].type), values=stmt.values[0])
-                                new_stmts.append(new_let_stmt)
-                        else:
-                            new_stmts.append(stmt)
-                    top_children.setBody(new_stmts)
-                    top.setBody(top_children)
-
-                elif isinstance(top_children, FunctionDef) and isinstance(top_children.body, Block):
-                    for stmt in top_children.body.getChildren():
-                        if self.utils.statements_eq(stmt, target_node):
-                            if len(stmt.var_defs) == 1:
-                                new_let_stmt = LetStmt(var_defs=VarDef(name=stmt.var_defs[0].name, 
-                                    mutable= not stmt.var_defs[0].mutable,
-                                    by_ref=stmt.var_defs[0].by_ref,
-                                    var_type=stmt.var_defs[0].type), values=stmt.values[0])
-                                new_stmts.append(new_let_stmt)
-                        else:
-                            new_stmts.append(stmt)
-                    top_children.body.setBody(new_stmts)
-                    top.setBody(top_children)
-                remaining_tops.append(top)
-        
-        print("flipped tree:", pretty_print_ast(Program(items=remaining_tops)))
+        print("safe struct:", pretty_print_ast(Program(items=remaining_tops)))
         return Program(items=remaining_tops)
 
     def shuffle_and_update_block(self, node, block):
@@ -183,53 +178,12 @@ class ReplacementOperator:
     def safe_wrap_raw_pointers(self, ast_root, target_node):
         return self.utils.transform_ast(ast_root, target_node, self.replace_raw_pointer_defs_with_safe_wrappers)
     
-    def safe_wrap_struct_field(self, ast_root, target_node):
-        if not isinstance(ast_root, Program):
-            return None
-        print("safe_wrap_struct_field")
-        remaining_tops = []
-        for top in ast_root.getChildren():
-            new_fields = []
-            if isinstance(top, StructDef):
-                for field in top.getChildren():
-                    print("a2", len(top.getChildren()))
-                    if isinstance(field.type, PointerType) and field.type.mutability:
-                        print("a3")
-                        new_field = StructField(name=field.name, typeExpr=SafeNonNullWrapper(
-                            typeExpr=field.type), visibility=field.visibility)
-                        new_fields.append(new_field)
-                    else:
-                        new_fields.append(field)
-                top.setChildren(new_fields)
-
-            elif isinstance(top, FunctionDef) and isinstance(top.body, Block):
-                new_Stmts = []
-                for stmt in top.body.getChildren():
-                    if isinstance(stmt, StructDef):
-                        for field in stmt.getChildren():
-                            if isinstance(field.type, PointerType) and field.type.mutability:
-                                new_field = StructField(name=field.name, typeExpr=SafeNonNullWrapper(
-                                    typeExpr=field.type), visibility=field.visibility)
-                                new_fields.append(new_field)
-                            else:
-                                new_fields.append(field)
-                        new_Stmts.append(new_field)
-                    else:
-                        new_Stmts.append(stmt)
-                new_block = Block(stmts=new_Stmts, isUnsafe=top.body.isUnsafe)
-                top.setBody(new_block)
-            remaining_tops.append(top)
-
-        print("safe struct:", pretty_print_ast(Program(items=remaining_tops)))
-        return Program(items=remaining_tops)
-
     def move_ast_node(self, ast_root, target_node):
         if not isinstance(target_node, Block):
             return ast_root
         return self.utils.transform_ast(ast_root, target_node, self.shuffle_and_update_block)
 
     def make_global_static_pointers_unmutable(self, ast_root, target_node):
-        # print("make_global_static_pointers_unmutable")
         if isinstance(target_node, TopLevel):
             top_items = []
             for top in ast_root.getChildren():
--- source/pyggi/sample/bst_rust/bst.rs
+++ source/pyggi/sample/bst_rust/bst.rs
@@ -1,11 +1,11 @@
 
 
 fn main() {
-    // let a : i32 = 1;
+    let a : i32 = 1;
     // pub struct List {
     //     pub s: *mut *mut State,
     //     pub n: libc::c_int,
     // }
-    let ptr : String = "Razie";
-    let val = *ptr;
+    // let ptr : String = "Razie";
+    // let val = *ptr;
 }
