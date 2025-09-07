bab0116293a4fc41374070c327f8051c0c87ee86
JSavree <jrliao2@illinois.edu>
2025-09-07 10:02:20 -0500

    "update some of the simulator structure"

--- source/RustParser/AST_Scripts/ast/Simulator.py
+++ source/RustParser/AST_Scripts/ast/Simulator.py
@@ -11,6 +11,7 @@ from TypeChecker import TypeChecker
 from collections import ChainMap
 from collections import deque
 
+from source.RustParser.AST_Scripts.ast.ProgramVisitor import ProgramVisitor
 from source.RustParser.AST_Scripts.ast.Expression import *
 
 # from types import NoneType
@@ -19,6 +20,7 @@ NoneType = type(None)
 
 # I need to add Box maybe?
 # I also may need to add arrays
+
 
 class Simulator(ProgramVisitor):
     # x, y, z, env : ChainMap{ x: n, y : m, z : v} , n m v are nat numbers 100, 100, 100, eg {x : 128}
@@ -33,35 +35,6 @@ class Simulator(ProgramVisitor):
         self.heap = memory
         self.stack = stack
         self.funMap = dict()
-        # self.heap = {}
-        # self.stack_bools = deque()
-
-        # The goal is to enable all the examples (see slack): aggregate.rs, bst.rs, nfa.rs
-        # aggregate and bst have some stuff that are missing
-        # i may want to model lifetimes as well
-        # start with doing the memory for the simulator, and later try and implement the lifetime stuff from rust
-
-        # I should also research rust ast: https://doc.rust-lang.org/nightly/nightly-rustc/rustc_ast/ast/index.html
-        # parser -> parse xml into rust, then run the simulator
-        # Rust_parser -- P --> AST_P (visitor pattern) --> print out (form a string) the XML form
-        # (you can decide how to print out) --> our_parser_will_parse the XML_string to XML_Rust -> use simulator.
-
-        # I should do array, array length?, function, reference type, reference operator, make
-        # if left as separate thing, make let statement (maybe)
-        # it's fine to have both match and if left.
-# xml generated code -> programmer
-#memory_life_time: variable -> position
-#memory : nat_number -> (offset -> value)
-#stack : variable -> value (nat_number, or some other stack value)
-#pointer a, a -> address_in_stack, address_in_stack -> memory_field,
-#memory_field == value, a is a pointer (associated with the number of data it allocated), memory_field = (value, number_of_fields)
-#value = list of_basic_data, number_field n, 0 -> value0 , 1 -> value1 , 2 -> value2, ...., *(a+i) --> memory_field_of_a with the offset_i
-
-# testing the simulator on real rust programs
-
-#--->  XML form of rust programs. how you can test on real rust?
-
-#Rust_parser -- P --> AST_P (visitor pattern) --> print out (form a string) the XML form (you can decide how to print out) --> our_parser_will_parse the XML_string to XML_Rust -> use simulator.
 
     def get_state(self):
         return self.memory
@@ -72,29 +45,24 @@ class Simulator(ProgramVisitor):
     def get_val(self):
         return self.heap
 
-    # I need to write the function of these grammar now, not just print them out
+    def visitLetStmt(self, node: LetStmt):
+        newNode = self.funMap.get(node.var_defs)
 
-    # Let statement should assign something?
-    # For now, implement some of the expressions
-    def visit_LetStmt(self, ctx: LetStmt):
-        x = ctx.var_defs # make idexp return identifier
-        y = ctx.values # exp will return the value
-        res = ctx.accept()
-        self.stack.update({str(x) : y})
-        return
+        newStack = self.stack.deepCopy()
 
-    #def visitMatch(self, ctx: XMLProgrammer.QXMatch):
-        # I will need to modify things even more or make an entirely new thing just for the match statement (due to
-        # the fact that its syntax is very different)
-    #    return
+        for i in len(newNode.params):
+            arVar = newNode.params[i]
+            value = node.args[i].accept(self)
+            newStack.update({arVar : value})
 
-    # def visitPrint(self, ctx: XMLProgrammer.QXPrint):
-    #     # this prints the stringval, then does something with the exp?
-    #     # This would call my printer, I will implement later (will be harder)
-    #     print(ctx.str())
-    #     return
+        oldStack = self.stack
+        self.stack = newStack
+        result = newNode.body.accept(self)
+        self.stack = oldStack
 
-    def visitFunctionDef(self, node:FunctionDef):
+        return result
+
+    def visitFunctionDef(self, node: FunctionDef):
         self.funMap.update({node.identifier : node})
         return None
 
@@ -112,34 +80,31 @@ class Simulator(ProgramVisitor):
         self.stack = oldStack
         return result
 
+    def visitIfStmt(self, node: IfStmt):
+        # newNode = self.funMap.get(node.var_defs)
 
-    def visitIfStmt(self, ctx: IfStmt):
-        if_result = ctx.accept(self) # .vexp()
-        result = None
+        if_result = node.condition
+
         if if_result:
-            result = ctx.then_branch
+            result = node.then_branch.accept(self)
         else:
-            result = ctx.else_branch
-
+            result = node.else_branch.accept(self)
 
         return result
 
-    def visit_Break(self, ctx: BreakStmt):
-        # This will be more complicated due to the different types of loops
-        self.stack_bools.pop()
-        self.stack_bools.append(False)
+    def visitBreakStmt(self, node: BreakStmt):
 
-        if ctx is not None: # .vexp()
-            return ctx.accept(self) # .vexp()
+        if node is not None: # .vexp()
+            return node.accept(self) # .vexp()
         return None # maybe this is better to return?
 
-    def visit_ReturnStmt(self, ctx: ReturnStmt):
-        if ctx.accept(self) is None:
+    def visitReturnStmt(self, node: ReturnStmt):
+        if node.accept(self) is None:
             return
         else:
-            return ctx.value
+            return node.value
 
-    def visit_LoopStmt(self, ctx: LoopStmt):
+    def visitLoopStmt(self, ctx: LoopStmt):
         # This is the loop keyword. For this type of loop, break statement can return a value
         # A loop statement contains a block statement, and if a break appears in the immediate block statement,
         # this loop will end?
@@ -158,7 +123,7 @@ class Simulator(ProgramVisitor):
 
         return None
 
-    def visit_ForStmt(self, ctx: ForStmt):
+    def visitForStmt(self, ctx: ForStmt):
         # This is the traditional for loop
         x = ctx.var
         v = self.visit(ctx.accept())
@@ -193,45 +158,49 @@ class Simulator(ProgramVisitor):
 
         return ctx.accept()
 
-    def visit_BinaryExpr(self, ctx: BinaryExpr):
-        operator = str(ctx.op)
+    def visitBinaryExpr(self, node: BinaryExpr):
+        oper = str(node.op)
         # This will be very complicated.
-        a = ctx.left
-        b = ctx.right
+        a = node.left.accept(self)
+        b = node.right.accept(self)
         # range is more complicated due to there being an = operator. I can forget about this case for now.
         # Now, I need to write out the cases for each operator.
 
-        if operator == '+':
+        if oper == '+':
             return a + b
-        elif operator == '-':
+        elif oper == '-':
             return a - b
-        elif operator == '*':
+        elif oper == '*':
             return a * b
-        elif operator == '/':
+        elif oper == '/':
             return a / b
-        elif operator == '%':
+        elif oper == '%':
             return a % b
         # elif operator == 'Exp':
         #     return pow(a, b)
-        elif operator == '&&':
+        elif oper == '&&':
             return a and b
-        elif operator == '||':
+        elif oper == '||':
             return a or b
-        elif operator == '<':
+        elif oper == '<':
             return a < b
-        elif operator == '>':
+        elif oper == '>':
             return a > b
-        elif operator == '<=':
+        elif oper == '<=':
             return a <= b
-        elif operator == '>=':
+        elif oper == '>=':
             return a >= b
-        elif operator == '==':
+        elif oper == '==':
             return a == b
-        elif operator == '!=':
+        elif oper == '!=':
             return a != b
             #return result
 
         return None
+
+    def visitBoxWrapperExpr(self, node: BoxWrapperExpr):
+
+        return
 
     # def visitVexp(self, ctx: XMLExpParser.VexpContext):
     #     if ctx.idexp() is not None:
