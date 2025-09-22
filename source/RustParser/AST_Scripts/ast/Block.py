8b272dfb1fb103f0c5aa4f2f5dab2a1278ffddc0
rosepose <rosepose@gmail.com>
2025-09-22 09:49:32 +0330

    fix: block was refactored

--- source/RustParser/AST_Scripts/ast/Block.py
+++ source/RustParser/AST_Scripts/ast/Block.py
@@ -1,14 +1,5 @@
 from RustParser.AST_Scripts.ast.ASTNode import ASTNode
 
-class InitBlock(ASTNode):
-    def __init__(self, attrList, returnExpr):
-        super().__init__()
-        self.attrList = attrList
-        self.returnExpr = returnExpr
-
-    def accept(self, visitor):
-        return visitor.visitInitBlock(self)
-
 class Block(ASTNode):
     def __init__(self, stmts, isUnsafe):
         self.stmts = stmts
