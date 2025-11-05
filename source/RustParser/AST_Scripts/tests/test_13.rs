a8d880c9cd1ee6c3e0b8983843661a652c931bd1
rose pose <rosepose@gmail.com>
2025-11-05 17:34:47 +0330

    feat: genprog passed the full defected bst successfully

--- source/RustParser/AST_Scripts/tests/test_13.rs
+++ source/RustParser/AST_Scripts/tests/test_13.rs
@@ -32,3 +32,43 @@ unsafe fn insert(root: *mut Node, key: i32, value: &str) -> *mut Node {
 
     return root;
 }
+
+unsafe fn search(root: *mut Node, key: i32) -> *const String {
+    let return_val = "None";
+    if root == None {
+        return return_val;
+    }
+
+    if key == (*root).key {
+        return_val = root.value;
+    }
+
+    if key < (*root).key {
+        return_val = search((*root).left, key);
+    }
+
+    if key > (*root).key {
+        return_val = search((*root).right, key);
+    }
+
+    return return_val;
+}
+
+fn main() {
+    unsafe {
+        let root = new_node(3, "three");
+        let three_found = search(root, 3);
+        root = insert(root, 5, "five");
+        let five_found = search(root, 5);
+        // insert(root, 7, "seven");
+        // search(root, 7);
+        // insert(root, 4, "four");
+        // search(root, 4);
+        // insert(root, 2, "two");
+        // search(root, 2);
+        // insert(root, 6, "six");
+        // search(root, 6);
+        // insert(root, 8, "eight");
+        // search(root, 8);
+    }
+}
